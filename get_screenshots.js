c21fa688a103b327d28f6f08de318b5014004c71
Steve Astels <sastels@gmail.com>
2018-08-28 16:14:36 -0400

    refactored and added date

--- get_screenshots.js
+++ get_screenshots.js
@@ -1,23 +1,22 @@
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY_2}).base('appMmtGZZe1kN10Uy');
-const Pageres = require('pageres');
+var Airtable = require("airtable");
+var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY_2}).base("appMmtGZZe1kN10Uy");
+const Pageres = require("pageres");
 const AWS = require("aws-sdk");
-const fs = require('fs');
+const fs = require("fs");
 
 AWS.config.update({
   region: "us-east-2",
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
-
 const s3 = new AWS.S3();
 
-const read_and_upload = (fname) => {
-  fs.readFile(fname, function (err, buffer) {
+const read_and_upload = (fname, key) => {
+  fs.readFile(fname, function(err, buffer) {
     if (err) throw err;
-    uploadFile(buffer, fname)
+    uploadFile(buffer, key);
   });
-}
+};
 
 const uploadFile = (buffer, key) => {
   const params = {
@@ -28,48 +27,61 @@ const uploadFile = (buffer, key) => {
     ContentType: "image/png"
   };
   s3.putObject(params, function(err) {
-      if (err) {
-          console.log(err)
-      } else {
-          console.log("Successfully uploaded data to vac-screenshots/" + key);
-   }
- });
-}
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully uploaded data to vac-screenshots/" + key);
+    }
+  });
+};
 
-const url2Fname = url => url.replace(/\//g, "-")
-  .replace("?", "-")
-  .replace("=", "-")
-  .replace(":", "-");
-
-const snapshot = (url) => {
-  fname = url2Fname(url)
-  new Pageres({delay: 2, filename: fname})
-    .src(url, ['1024x768'])
+const snapshot = (project, url, fileName, date) => {
+  if (date === undefined) {
+    const now = new Date();
+    date = [
+      now.getFullYear(),
+      now.getMonth() + 1,
+      now.getDate()
+    ].join("-");
+  }
+  const fname = fileName + "-" + date;
+  const key = project + "/" + fileName + "/" + fname + ".png"
+  new Pageres({
+    delay: 2,
+    filename: fname
+  })
+    .src(url, ["1024x768"])
     .dest(__dirname + "/img")
     .run()
     .then(() => {
-      console.log("took snapshot of", url)
-      read_and_upload(__dirname + "/img/" + fname + ".png")
+      console.log("took snapshot of", url);
+      console.log("saved locally:", "img/"+ fname + ".png");
+      read_and_upload(__dirname + "/img/" + fname + ".png", key)
     });
+};
+
+/****** Main ******/
+
+let date = undefined
+if (process.argv.length === 3) {
+  date = process.argv[2]
 }
 
-
-base('urls').select({
-    maxRecords: 100,
-    view: "Grid view"
+base("urls").select({
+  maxRecords: 100,
+  view: "Grid view"
 }).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-    records.forEach(function(record) {
-      const url = record.get('url')
-        console.log('Airtable:', url);
-        snapshot(url)
-    });
-
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
+  records.forEach(function(record) {
+    const project = record.get("project")
+    const url = record.get("url");
+    const fileName = record.get("fileName")
+    console.log("Airtable:", project, url, fileName);
+    snapshot(project, url, fileName, date);
+  });
+  fetchNextPage();
 
 }, function done(err) {
-    if (err) { console.error(err); return; }
+  if (err) {
+    console.error(err);
+  }
 });
